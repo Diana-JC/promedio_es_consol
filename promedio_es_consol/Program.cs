aa4e67f1f68410562efc7b9c26c40f2de586e758
dc552 <dc552@DESKTOP-80ATPO4>
2024-02-12 19:38:23 -0400

    Agregar archivos de proyecto.

---
+++ promedio_es_consol/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Calculo del promedio");
+Console.WriteLine();
+
+
+int Nota1, Nota2, Nota3, Nota4;
+
+Console.WriteLine("Ingresar la Nota1: ");
+Nota1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingresar la Nota2: ");
+Nota2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingresar la Nota3: ");
+Nota3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingresar la Nota4: ");
+Nota4 = Convert.ToInt32(Console.ReadLine());
+
+
+double Nota = (Nota1 + Nota2 + Nota3 + Nota4) / 4;
+
+if (Nota > 70)
+
+{
+    Console.WriteLine("El Estudiente Aprobo");
+}
+else
+{
+    Console.WriteLine("El Estudiante Reprobo");
+}
